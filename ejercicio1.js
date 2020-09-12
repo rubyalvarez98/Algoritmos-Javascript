697d0ce202a0c28aa61d57197f43acc73a7867ea
Ruby Alvarez Restrepo <alvarezruby098@gmail.com>
2020-09-11 20:04:57 -0500

    aprobado

---
+++ ejercicio1.js
@@ -0,0 +1,4 @@
+/*1. Dada la calificación de un alumno mostrar en pantalla si
+ es aprobado siempre y cuando tenga el 80% de sus asistencias, en caso de tener menos del 80% de asistencias 
+se mostrará reprobado aún cuando su calificación sea aprobatoria */
+
