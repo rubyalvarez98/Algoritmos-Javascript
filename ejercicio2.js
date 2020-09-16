3310e21f2925ac653b2da6c24e0fa8459723f44e
Ruby Alvarez Restrepo <alvarezruby098@gmail.com>
2020-09-15 20:58:58 -0500

    le quito los console.log y lo agrego fuera de la función

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,15 +1,16 @@
 // Leer tres números y decir cuál de ellos es el que tiene el valor intermedio
 
-function numero(num1,num2,num3){
+function intermedio(num1,num2,num3){
 
 if((num1 < num2 && num1 > num3) || (num1>num2 && num1<num3)){
-   return console.log(`El numero intermedio es ${num1}`);
+   return `El numero intermedio es ${num1}`;
 
 }else if((num2 > num1 && num2 < num3) ||(num2 <num1 && num2 > num3)){
-   return console.log(`El numero intermedio es ${num2}`);
+   return `El numero intermedio es ${num2}`;
 }else{
-     return console.log(`El numero intermedio es ${num3}`); 
+     return `El numero intermedio es ${num3}`; 
 }
 
 }
-numero(5,1,3)
+var resultado = intermedio(30,15,25);
+console.log (resultado);
