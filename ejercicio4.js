5301deb03b19bce273dca33e057060ce1268a321
Ruby Alvarez Restrepo <alvarezruby098@gmail.com>
2020-09-15 19:51:34 -0500

    correccion

--- ejercicio4.js
+++ ejercicio4.js
@@ -1,10 +1,10 @@
 // Leer un número entre el 1 y el 10 e imprimir su equivalente en números romanos
 
-const num = 5;
+function romanos(num){
 
 switch(num) {
-case 1: console.log("El numero romano de 1 es: I"); break;
-case 2: console.log("El numero romano de 2 es: II"); break;
+case 1: return("El numero romano de 1 es: I"); break;
+case 2: ("El numero romano de 2 es: II"); break;
 case 3: console.log("El numero romano de 3 es: III"); break;
 case 4: console.log("El numero romano de 4 es: IV"); break;
 case 5: console.log("El numero romano de 5 es: V"); break;
@@ -15,3 +15,6 @@ case 9: console.log("El numero romano de 9 es: IX"); break;
 case 10: console.log("El numero romano de 10 es: X"); break;
 default: console.log("El numero ingresado es invalido"); break;
 }
+}
+var romano = romanos(3);
+console.log (romano);
