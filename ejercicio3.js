2d8ff4de75a69dbb2d63f3158e3886e4373dbceb
Ruby Alvarez Restrepo <alvarezruby098@gmail.com>
2020-09-15 21:44:27 -0500

    correccion de variable

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,26 +1,27 @@
 /*Leer tres números y decir cuál es el de valor mayor, 
 el de valor intermedio, y el de valor menor*/
 
-function numero(a,b,c){
+function orden(a,b,c){
 
 if(a>b && b<c){
-     return console.log(`El orden de mayor a menor: ${a}, ${b}, ${c}`);
+     return `El orden de mayor a menor: ${a}, ${b}, ${c}`;
 
 }else if(a>c && c>b){
-   return console.log(`El orden de mayor a menor: ${a}, ${c}, ${b}`);
+   return `El orden de mayor a menor: ${a}, ${c}, ${b}`;
 
 }else if(b>a && a>c){
-   return console.log(`El orden de mayor a menor: ${b}, ${a}, ${c}`);
+   return `El orden de mayor a menor: ${b}, ${a}, ${c}`;
 
 }else if(b>c && c>a){
-    return console.log(`El orden de mayor a menor: ${b}, ${c}, ${a}`);
+    return `El orden de mayor a menor: ${b}, ${c}, ${a}`;
 
 }else if(c>b && b>a){
-   return console.log(`El orden de mayor a menor: ${c}, ${b}, ${a}`);
+   return `El orden de mayor a menor: ${c}, ${b}, ${a}`;
 
 }else {
-       return console.log(`El orden de mayor a menor: ${c}, ${a}, ${b}`);
+       return `El orden de mayor a menor: ${c}, ${a}, ${b}`;
 
 }
 }
-numero(30,45,20)
+ let mayorAMenor= orden(30,45,20);
+ console.log(mayorAMenor);
