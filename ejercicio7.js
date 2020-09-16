d505a81119f7c565bb824a0915912245bf7bfe87
Ruby Alvarez Restrepo <alvarezruby098@gmail.com>
2020-09-15 22:17:10 -0500

    resta

--- ejercicio7.js
+++ ejercicio7.js
@@ -1,13 +1,15 @@
 //Leer dos números enteros, restar el menor del mayor y mostrar el resultado
 
-function numeros(num1,num2) {
-
+function resta(num1,num2) {
+    
+    
     if (num1 > num2) {
-         const resultado = num1 -num2;
+         return `El resultado es: ${num1 - num2}`;
         
-    } else {
-        resultado = num2 - num1;
+    } else 
+        return `El resultado es: ${num2-num1}`;
+    
     }
-    return console.log(`El resultado es: ${resultado}`);
-}
-numeros(6,9)
+    
+let resultado = resta(6,9);
+console.log(resultado);
