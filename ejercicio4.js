d99d8cb7675a6f63b414a0112009e9d3db80f3b2
Ruby Alvarez Restrepo <alvarezruby098@gmail.com>
2020-09-15 19:59:58 -0500

    le quite los break porque con el return no es necesario

--- ejercicio4.js
+++ ejercicio4.js
@@ -3,17 +3,17 @@
 function romanos(num){
 
 switch(num) {
-case 1: return("El numero romano de 1 es: I"); break;
-case 2: ("El numero romano de 2 es: II"); break;
-case 3: console.log("El numero romano de 3 es: III"); break;
-case 4: console.log("El numero romano de 4 es: IV"); break;
-case 5: console.log("El numero romano de 5 es: V"); break;
-case 6: console.log("El numero romano de 6 es: VI"); break;
-case 7: console.log("El numero romano de 7 es: VII"); break;
-case 8: console.log("El numero romano de 8 es: VIII"); break;
-case 9: console.log("El numero romano de 9 es: IX"); break;
-case 10: console.log("El numero romano de 10 es: X"); break;
-default: console.log("El numero ingresado es invalido"); break;
+case 1: return("El numero romano de 1 es: I");
+case 2: ("El numero romano de 2 es: II");
+case 3: console.log("El numero romano de 3 es: III");
+case 4: console.log("El numero romano de 4 es: IV");
+case 5: console.log("El numero romano de 5 es: V");
+case 6: console.log("El numero romano de 6 es: VI");
+case 7: console.log("El numero romano de 7 es: VII"); 
+case 8: console.log("El numero romano de 8 es: VIII");
+case 9: console.log("El numero romano de 9 es: IX");
+case 10: console.log("El numero romano de 10 es: X");
+default: console.log("El numero ingresado es invalido");
 }
 }
 var romano = romanos(3);
